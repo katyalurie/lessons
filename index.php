b40f56163ffda2baedbee0a1112fb637910d7abf
katyalurie <lurie.katya@mail.ru>
2016-10-15 16:53:53 +0300

    Первый коммит!

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+function pripev (){
+    echo "припев строка 1<br />";
+    echo "припев строка 2<br />";
+    echo "припев строка 3<br />";
+    echo "припев строка 4<br /><br />";
+}
+
+echo "Куплет 1<br /><br />";
+pripev();
+echo "Куплет 2<br /><br />";
+pripev();
+echo "Куплет 3<br /><br />";
+pripev();
+echo "Куплет 4<br /><br />";
+pripev();
+
+
+?>
